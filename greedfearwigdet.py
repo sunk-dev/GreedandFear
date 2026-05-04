62fbd2a6a7cdebbb24c78a59b59d76e96a5745a6
seonkyoung <103514806+sunk-dev@users.noreply.github.com>
2026-05-04 16:49:24 +0900

    Update greedfearwigdet.py

--- greedfearwigdet.py
+++ greedfearwigdet.py
@@ -1,5 +1,23 @@
 import fear_and_greed
+import streamlit as st
 def get_fing_data():
     index_data = fear_and_greed.get()
     return index_data
-print(get_fing_data())
+# return data
+'''
+FearGreedIndex(value=66.6, description='greed', last_update=datetime.datetime(2026, 5, 1, 23, 59, 39, tzinfo=datetime.timezone.utc))
+'''
+
+# page setting
+st.set_page_config(page_title="F&G Index Wiget", layout="centered")
+index_data=fear_and_greed.get()
+value=int(index_data.value)
+status=index_data.description
+
+#wiget design
+st.title("Feat& Greed Index")
+st.metric(label=f"Status: {status}",value=value)
+if value<25:
+    st.error("Extreme Fear")
+elif value>75:
+    st.success("Extreme Greed")
