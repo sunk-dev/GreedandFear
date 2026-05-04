d1b405fa2e9faa22623d08017187b486b6ab5fcf
seonkyoung <103514806+sunk-dev@users.noreply.github.com>
2026-05-04 17:14:59 +0900

    Update greedfearwigdet.py

--- greedfearwigdet.py
+++ greedfearwigdet.py
@@ -18,9 +18,9 @@ date=datetime.now()
 st.title(f"Fear& Greed Index: {date.date()}")
 if value<25 or (value>25 and value<45):
     # st.error("Extreme Fear")
-    st.metric(label=f"Status: :red[{status}]",value=value)
+    st.metric(label=f"Status: :red[{status}]",value=f":red[{value}]")
 
 elif value>75 or (value>55 and value<75):
     # st.success("Extreme Greed")
-    st.metric(label=f"Status: :green[{status}]",value=value)
+    st.metric(label=f"Status: :green[{status}]",value=f":green[{value}]")
 
